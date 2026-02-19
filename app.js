cd22ab7635b419e2214d55f00b6f54d26378e9f1
Krasimir Kazakov <kkazakov@gmail.com>
2026-02-19 12:15:37 +0200

    scroll to bottom when sending a message

--- app.js
+++ app.js
@@ -1029,7 +1029,7 @@ y1: {
                 }
 
                 this.newMessage = '';
-                await this.fetchMessages();
+                await this.fetchMessages({ forceScrollToBottom: true });
             } catch (err) {
                 console.error(err);
             } finally {
